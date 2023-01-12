bbe62b730453cf39e8e3be733e6df760739699ac
WilliamC <WilliamNCalda@gmail.com>
2023-01-12 19:38:16 +0800

    Add Background Image and Center Button

--- index.php
+++ index.php
@@ -14,36 +14,39 @@
 	<link rel="icon" type="icon/x-image" href="src/rmi-icon.jpg">
     <title>RMI Project</title>
 </head>
-	<body>
+<body>
+	<div class="bg-image" style="background-image: url('src/rmi-bg.png'); height: 100vh; background-repeat: no-repeat; background-position: 40% center;">
 	<div class="card-header">
-        <h4>RMI Project</h4>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 mt-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Upload Riders Data</h4>
-                    </div>
-                    <div class="card-body">
+		<h4>RMI Project</h4>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 mt-4">
+				<div class="card">
+					<div class="card-header">
+						<h4>Upload Riders Data</h4>
+					</div>
+					<div class="card-body">
 
-                        <form action="" method="POST" enctype="multipart/form-data">
+						<form action="" method="POST" enctype="multipart/form-data">
 
-                            <input type="file" name="excel" class="form-control" value="" required/>
-                            <button type="submit" name="import" class="btn btn-primary mt-3">Import</button>
+							<input type="file" name="excel" class="form-control" value="" required/>
+							<div class="text-center">
+								<button type="submit" name="import" class="btn btn-primary mt-3">Import</button>
+							</div>
 
-                        </form>
+						</form>
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>  
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>  
 	<?php
 		if(isset($_POST["import"])){
 			$fileName = $_FILES["excel"]["name"];
 			$fileExtension = explode('.', $fileName);
-      		$fileExtension = strtolower(end($fileExtension));
+			$fileExtension = strtolower(end($fileExtension));
 			$newFileName = date("Y.m.d") . " - " . date("h.i.sa") . "." . $fileExtension;
 
 			$targetDirectory = "uploads/" . $newFileName;
@@ -71,5 +74,6 @@
 		}
 	?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-	</body>
+	</div>
+</body>
 </html>
