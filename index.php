9dd691945e5a554e6da6a0ce4111d603530b89b7
WilliamC <WilliamNCalda@gmail.com>
2023-01-13 11:09:47 +0800

    Login Added NoPHPScript Yet

--- index.php
+++ index.php
@@ -16,63 +16,64 @@
 </head>
 <body>
 	<div class="bg-image" style="background-image: url('src/rmi-bg.png'); height: 100vh; background-repeat: no-repeat; background-position: center;">
-	<div class="card-header">
-		<h4>RMI Project</h4>
-	</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 mt-4">
-				<div class="card">
-					<div class="card-header">
-						<h4>Upload Riders Data</h4>
-					</div>
-					<div class="card-body">
+		<div class="card-header">
+			<h4>RMI Project</h4>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 mt-4">
+					<div class="card">
+						<div class="card-header">
+							<h4>Upload Riders Data</h4>
+						</div>
+						<div class="card-body">
 
-						<form action="" method="POST" enctype="multipart/form-data">
+							<form action="" method="POST" enctype="multipart/form-data">
 
-							<input type="file" name="excel" class="form-control" value="" required/>
-							<button type="submit" name="import" class="btn btn-primary mt-3">Import</button>
+								<input type="file" name="excel" class="form-control" value="" required/>
+								<button type="submit" name="import" class="btn btn-primary mt-3">Import</button>
 
-						</form>
+							</form>
 
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>  
-	<?php
-		if(isset($_POST["import"])){
-			$fileName = $_FILES["excel"]["name"];
-			$fileExtension = explode('.', $fileName);
-			$fileExtension = strtolower(end($fileExtension));
-			$newFileName = date("Y.m.d") . " - " . date("h.i.sa") . "." . $fileExtension;
+		</div>  
+		<?php
+			if(isset($_POST["import"])){
+				$fileName = $_FILES["excel"]["name"];
+				$fileExtension = explode('.', $fileName);
+				$fileExtension = strtolower(end($fileExtension));
+				$newFileName = date("Y.m.d") . " - " . date("h.i.sa") . "." . $fileExtension;
 
-			$targetDirectory = "uploads/" . $newFileName;
-			move_uploaded_file($_FILES['excel']['tmp_name'], $targetDirectory);
-			error_reporting(0);
-			ini_set('display_errors', 0);
+				$targetDirectory = "uploads/" . $newFileName;
+				move_uploaded_file($_FILES['excel']['tmp_name'], $targetDirectory);
+				error_reporting(0);
+				ini_set('display_errors', 0);
 
-			require 'excelReader/excel_reader2.php';
-			require 'excelReader/SpreadsheetReader.php';
+				require 'excelReader/excel_reader2.php';
+				require 'excelReader/SpreadsheetReader.php';
 
-			$reader = new SpreadsheetReader($targetDirectory);
-			foreach($reader as $key => $row){
-				$ptname = $row[0];
-				$issuedate = $row[1];
-				$amount = $row[2];
-				mysqli_query($conn, "INSERT INTO partners VALUES('', '$ptname', '$issuedate', '$amount')");
+				$reader = new SpreadsheetReader($targetDirectory);
+				foreach($reader as $key => $row){
+					$ptname = $row[0];
+					$issuedate = $row[1];
+					$amount = $row[2];
+					
+					mysqli_query($conn, "INSERT INTO partners VALUES('', '$ptname', '$issuedate', '$amount')");
+				}
+
+				echo
+				"
+				<script>
+					alert('Succesfully Imported');
+					document.location.href = '';
+				</script>
+				";
 			}
-
-			echo
-			"
-			<script>
-				alert('Succesfully Imported');
-				document.location.href = '';
-			</script>
-			";
-		}
-	?>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+		?>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 	</div>
 </body>
 </html>
---
+++ src/login.php
@@ -0,0 +1,56 @@
+<?php
+
+    require 'configuration.php';
+
+    // Check Connection
+    /*
+    if(mysqli_connect_errno()){
+        echo "Connection Error";
+    } else {
+        echo "Good Connection";
+    }
+    */
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="icon" type="icon/x-image" href="src/rmi-icon.jpg">
+    <title>Rider</title>
+</head>
+<body>
+    <div class="card-header">
+        <h4 class="text-center">Login</h4>
+    </div>
+    <div class="container">
+        <div class="col-md-24 mt-4">
+            <h4 class="text-center">EXSPEED Riders</h4>
+        </div>
+        <div class="col d-flex justify-content-center">
+            <div class="card bg-dark mb-3 w-40">
+                <div class="card-body text-center m-0">
+                    <div class="text-center m-1">
+                        <input class="text-center" type="text" placeholder="Rider Code">
+                        </div>
+                        <div class="text-center m-1">
+                        <input class="text-center" type="text" placeholder="Password">
+                    </div>
+
+                    <div class="card-body text-center">
+                        <button type="button" class="btn bg-light border-dark text-center">Sign In</button>
+                    </div>      
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
