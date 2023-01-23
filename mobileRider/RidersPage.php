a07497854583b5b9ff2d3c2de51603ff0b8745bd
WilliamC <WilliamNCalda@gmail.com>
2023-01-23 14:20:22 +0800

    Visual Changes and Added JS

--- mobileRider/RidersPage.php
+++ mobileRider/RidersPage.php
@@ -14,110 +14,132 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="styles.css">
+    <script src="App.js" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="App.js" type="module" defer></script>
     <title>Rider</title>
 </head>
 <body style="background-color: #FFF;">
-    <header class="text-wrap fixed-top bg-primary text-light d-flex justify-content-center ">
-        <h1 class="mt-2">
-            <?php 
-                session_start();
-                $RiderName = $_SESSION['RidersName'];   
-                echo $RiderName;
-            ?>
-        </h1>
-    </header>
-    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh">
-        
-        <div class="table-responsive" style="width: 300px;">
-            <table class="mx-auto">
-                <form action="" method="GET">
-                    <tr>
-                        <td>
-                            <label for="start_date" class="mt-5">From Date:</label>
-                            <input type="date" class="form-control d-block mb-2" id="fromdate" name="fromdate" min="2022-01-01" max="2023-12-31" value="2022-01-01">
-                        </td>
-                        <td>
-                            <label for="start_date" class="mt-5">To Date:</label>
-                            <input type="date" class="form-control d-block mb-2" id="todate" name="todate" min="2022-01-01" max="2023-12-31" value="2022-01-01">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" class="text-center">
-                            <button type="submit" class="mb-2 btn btn-primary">Generate</button>
-                        </td>
-                        <?php
-                            
-                        if(isset($_GET['fromdate']) && isset($_GET['todate'])){
-                            $fromdate = $_GET['fromdate'];
-                            $todate = $_GET['todate'];
-                            $dsql = "SELECT * FROM datarider WHERE RiderName = '$RiderName' AND CreditDate BETWEEN '$fromdate' AND '$todate'";
-                            $datequery = $conn->query($dsql);
+    <div class="container-fluid text-center bg-primary text-light">
+        <div class="row">
+            <div class="col">
+            <h1>
+                <?php 
+                    session_start();
+                    $RiderName = $_SESSION['RidersName'];   
+                    echo $RiderName;
+                ?>
+            </h1>
             
-                        ?>
-                    </tr>
-                </form>
-            </table>
-            <table class="table mx-auto">
-                <tr class="text-center">
-                    <th>Credit Date</th>
-                    <th>Amount</th>
-                </tr>
-                <?php
-                    if($datequery->num_rows > 0){
-                        while($row = $datequery->fetch_assoc()){
-                            echo
-                                "<tr class=\"text-center\">
-                                    <td>" . $row["CreditDate"]. " </td>
-                                    <td>" . $row["AmountIssued"]. "</td>
-                                </tr>";
-                        }
-                    } else{
-                        echo "<td colspan=\"2\"> No Result Found </td>";
-                    }
-                } else {
-                ?>
-                <table class="table mx-auto">
-                    <tr class="text-center">
-                        <th>Credit Date</th>
-                        <th>Amount</th>
-                    </tr>
-                <?php
-                    $dsql = "SELECT * FROM datarider WHERE RiderName = '$RiderName'";
-                    $result = $conn->query($dsql);
-                    if ($result->num_rows > 0) {
-                        while($row = $result->fetch_assoc()) {
-                            echo 
-                            "<tr class=\"text-center\">
-                                <td>" . $row["CreditDate"]. " </td>
-                                <td>" . $row["AmountIssued"]. "</td>
-                            </tr>";
-                        }
-                    } else if(!$result){
-                        die("Query failed: " . mysqli_error($conn));
-                    }
-                }
-                ?>
-                <tr>
-                    <td class="text-right fw-bold">Total Amount</td>
-                    <td class="text-center">
-                    <?php 
-
-                        $nresult = $conn->query($dsql);
-                        $ntotal = 0;
-                        while($row = $nresult->fetch_assoc()) {
-                            $ntotal += $row['AmountIssued'];
-                        }
-
-                        echo $ntotal;
-                    ?>
-                    </td>
-                </tr>
-            </table>
+            </div>
         </div>
-    </div>  
-    <footer class="text-wrap mb-2 fixed-bottom d-flex justify-content-center">
+    </div>
+    
+    <img src="../src/XSpeedLogo.png" class="w-50 h-50 img-fluid rounded mx-auto d-block" alt="X Speed Logo">
+    
+    <div class="container d-flex justify-content-center">
+        <form action="" method="GET">
+            <div class="row g-1">
+                <div class="col">
+                    <label for="start_date">From Date:</label>
+                    <input type="date" class="form-control" id="fromdate" name="fromdate" min="2022-01-01" max="2023-12-31" value="2022-01-01"> 
+                </div>
+                <div class="col">
+                    <label for="start_date">To Date:</label>
+                    <input type="date" class="form-control" id="todate" name="todate" min="2022-01-01" max="2023-12-31" value="2022-01-01">
+                </div>
+                <div class="col">
+                    <button type="submit" class="btn btn-primary mt-4">&#10004;</button>
+                </div>
+                <?php               
+                    if(isset($_GET['fromdate']) && isset($_GET['todate'])){
+                        $fromdate = $_GET['fromdate'];
+                        $todate = $_GET['todate'];
+                        $dsql = "SELECT * FROM datarider WHERE RiderName = '$RiderName' AND CreditDate BETWEEN '$fromdate' AND '$todate'";
+                        $datequery = $conn->query($dsql);
+                ?> 
+            </div>
+        </form>
+    </div> 
+
+
+    <div class="container justify-content-center">
+        <div class="row mb-2">
+            <div class="col mt-2 text-center fw-bold">
+                Credit Date
+            </div>
+            <div class="col mt-2 text-center fw-bold">
+                Amount
+            </div>
+        </div>
+            <?php
+                if($datequery->num_rows > 0){
+                    while($row = $datequery->fetch_assoc()){
+                        echo
+                        "<div class=\"row\">
+                            <div class=\"col text-center f-col\">" . $row["CreditDate"]. " </div>
+                            <div class=\"col text-center s-col\">" . $row["AmountIssued"]. "</div>
+                        </div>
+                        ";
+                    }
+                } else{
+                    echo "<td class=\"col\" colspan=\"2\"> No Result Found </td>";
+                }
+                } else {
+            ?>
+    </div>      
+    
+    <div class="container justify-content-center">
+        <div class="row mb-2">
+            <div class="col mt-2 text-center fw-bold">
+                Credit Date
+            </div>
+            <div class="col mt-2 text-center fw-bold">
+                Amount
+            </div>
+        </div>
+        <?php
+            $dsql = "SELECT * FROM datarider WHERE RiderName = '$RiderName'";
+            $result = $conn->query($dsql);
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo 
+                    "<div class=\"row\">
+                        <div class=\"col text-center f-col\">" . $row["CreditDate"]. " </div>
+                        <div class=\"col text-center s-col\">" . $row["AmountIssued"]. "</div>
+                    </div>
+                    ";
+                    }
+                } else if(!$result){
+                    die("Query failed: " . mysqli_error($conn));
+                }
+            }
+        ?>
+    </div>
+
+    <div class="container justify-content-center">
+        <div class="row mt-2">
+            <div class="col mt-2 fw-bold">
+                Total Amount
+            </div>
+            <div class="col mt-2 text-center">
+                <?php 
+
+                    $nresult = $conn->query($dsql);
+                    $ntotal = 0;
+                    while($row = $nresult->fetch_assoc()) {
+                        $ntotal += $row['AmountIssued'];
+                    }
+
+                    echo $ntotal;
+                ?>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+        <footer class="text-wrap mb-2 fixed-bottom d-flex justify-content-center">
             <button type="button" onclick="redirectToPage()" class="btn w-75 btn-primary">Logout</button>
             <script>
                 function redirectToPage() {
@@ -125,6 +147,10 @@
                 }
             </script>
         </footer> 
+    </div>
+       
+
+    <script src="App.js"></script>
 </body>
 </html>
 
